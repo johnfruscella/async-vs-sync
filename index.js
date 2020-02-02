ebbaf328e4024af7dad718d53d625908071fbe56
johnfruscella <johnfruscella@gmail.com>
2020-02-01 21:39:18 -0500

    replaced promises with async/await code

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 console.log('Before');
 //getUser(1, getRepositories);
-getUser(1)
-.then(user => getRepositories(user.gitHubUsername))
-.then(repos => getCommits(repos[0]))
-.then(commits => console.log('Commits', commits))
-.catch(err => console.log('Error', err.message));
+
+//Promise-based approach
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('Commits', commits))
+//     .catch(err => console.log('Error', err.message));
+
+// Async and Await approach  Uses Try/Catch instead of just catch for errors
+async function displayCommits() {
+    try{
+    const user = await getUser(1);
+    const repos = getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+} catch (err) {
+    console.log('Error', err.message);
+}  
+};
+displayCommits();
+
 console.log('After');
 
 // Callbacks are functions that will be called when the result of an asynchronos operation is ready.
 // Promises hold the eventual result of an asynchronous operation
-// Async/await
+// Async/await helps write asynchronous code like synchonos code. It is built on top of promises.
 
 function getUser(id) {
     return new Promise((resolve, reject) => {
@@ -26,6 +42,7 @@ function getRepositories(username) {
         setTimeout(() => {
             console.log('Calling GitHub API...');
             resolve(['repo1', 'repo2', 'repo3']);
+           // reject(new Error("Could not get the repositories"));
         }, 2000);
     });
 
@@ -33,7 +50,7 @@ function getRepositories(username) {
 
 function getCommits(repo) {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
+        setTimeout(() => {
             console.log('Calling the GITHUB API...');
             resolve(['commit']);
         }, 2000);
